ac4b3b31455185d8fe178bf382f76e7bcec8d67f
Paulina Gajewska <gajewska.paulina08@gmail.com>
2024-11-13 22:19:01 +0100

    Add images action

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@
 
 {
   'use strict';
-
+//  obiekt zawierający selektory, które będą nam potrzebne w tym module
 const select = {
   templateOf: {
     menuProduct: "#template-menu-product",
@@ -35,6 +35,7 @@ const select = {
     },
   };
 
+//  nazwy klas, którymi nasz skrypt będzie manipulował (nadawał i usuwał)  
   const classNames = {
     menuProduct: {
       wrapperActive: 'active',
@@ -42,6 +43,7 @@ const select = {
     },
   };
 
+//  ustawienia naszego skryptu, wszystkie wartości, które wygodniej będzie zmieniać w jednym miejscu
   const settings = {
     amountWidget: {
       defaultValue: 1,
@@ -50,6 +52,7 @@ const select = {
     }
   };
 
+//  szablony Handlebars, do których wykorzystujemy selektory z obiektu select
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
@@ -97,6 +100,7 @@ const select = {
       //console.log(thisProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       //console.log(thisProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -160,25 +164,39 @@ const select = {
         //determine param value, e.g. paramId= 'toppings', param = {label: 'Toppings', type: 'checkboxes'...}
         const param = thisProduct.data.params[paramId]; // param - cały obiekt, paramId - tylko nazwa parametru
         //console.log(paramId, param);
+        
         // for every option in this category - dla każdej opcji w kategorii
-        // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           //console.log(optionId, option);
-
+          const optionImageClass = '.'+ paramId + '-' + optionId;
+          //console.log(optionImageClass);
+          
+          const optionImage = thisProduct.imageWrapper.querySelector(optionImageClass);
           // check if there is param with a name of paramId in formData and if it includes optionId
           if(formData[paramId] && formData[paramId].includes(optionId)) {
             // check if the option is not default
             if(option.default!='true') {
               price += option.price;
+            }else {
+              // check if the option is default
+              if(option.default=='true') {
+                price -= option.price;
+              }
             }
-          } else {
-            // check if the option is default
-            if(option.default=='true') {
-              price -= option.price;
+            
+            if(optionImage){
+              //console.log(optionImage);
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
             }
           }
+          else{
+            if(optionImage){
+              //console.log(optionImage);
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          } 
         }
       }
       //update calculated price in the html
@@ -186,6 +204,7 @@ const select = {
     }
   }
 
+//  obiekt, który pomoże nam w organizacji kodu naszej aplikacji
   const app = {
     initMenu: function(){
       const thisApp = this;
@@ -215,5 +234,7 @@ const select = {
       thisApp.initMenu();
     },
   };
+
+//  wywołanie metody, która będzie uruchamiać wszystkie pozostałe komponenty strony
   app.init();
 }
